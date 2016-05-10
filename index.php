eb5ae612333131ab539319e85b1084bfbbd8af3f
Trevor Winsett <twinsettdev@outlook.com>
2016-05-10 09:29:52 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+  include_once 'resources/includes/header.php';
+
+?>
+
+<div id="mainB">
+  <div id="mainBT">
+    <h1>You're title</h1>
+  </div>
+  <div id="mainBC">
+    <p>You're content</p>
+  </div>
+</div>
+
+
+
+<?php
+  $page = "Home";
+  include_once 'resources/includes/footer.php';
+
+?>
