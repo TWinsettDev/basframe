27b23e7b817833aa7c30632c5a6464cb7cff97c5
Trevor Winsett <twinsettdev@outlook.com>
2016-05-10 09:16:24 -0600

    Create header.php

---
+++ resources/includes/header.php
@@ -0,0 +1,9 @@
+<?php
+  require("resources/scripts/php/main.php");
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link type="text/css" rel="stylesheet" src="/resources/style/main.css" />
+  </head>
+  <body>
